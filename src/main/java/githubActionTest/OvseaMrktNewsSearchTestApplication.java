ee419d7bae9bc8c4a36eb4e9635603c711724ba0
DESKTOP-F4BC13L\hi <jlee38266@gmail.com>
2024-06-27 12:46:50 +0900

    [feat] first commit

---
+++ src/main/java/githubActionTest/OvseaMrktNewsSearchTestApplication.java
@@ -0,0 +1,13 @@
+package githubActionTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvseaMrktNewsSearchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvseaMrktNewsSearchTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/githubActionTest/OvseaMrktNewsSearchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package githubActionTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvseaMrktNewsSearchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
