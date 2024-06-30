fce09c282d52a10e99a859bf1debfd685838772b
JaewonLee <jlee38266@gmail.com>
2024-06-30 17:55:36 +0900

    editing workflows inner files and main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,47 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # 서울 리전의 Amazon Linux 2 AMI
-  instance_type = "t2.micro"               # 프리 티어 인스턴스 유형
+resource "aws_opensearch_domain" "search_engine" {
+  domain_name           = "search-engine"
+  engine_version        = "OpenSearch_2.13" # OpenSearch 버전을 명시합니다.
+
+  cluster_config {
+    instance_type = "t2.small.search"  # 프리 티어 인스턴스 유형
+    instance_count = 1
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
+  access_policies = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:ap-northeast-2:123456789012:domain/search-engine/*",
+      "Condition": {
+        "IpAddress": {
+          "aws:SourceIp": "0.0.0.0/0"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
 
   tags = {
-    Name = "AppServer"
+    Domain = "search-engine"
   }
 }
