dcd8833393692a73944fd517ea1defd707714182
JaewonLee <jlee38266@gmail.com>
2024-06-30 15:54:02 +0900

    [rename] 프로젝트 디렉토리 이름 변경

--- src/main/java/githubActionTest/OpenSearchCICDTest.java
+++ src/main/java/githubActionTest/OpenSearchCICDTest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OvseaMrktNewsSearchTestApplication {
+public class OpenSearchCICDTest {
 
     public static void main(String[] args) {
-        SpringApplication.run(OvseaMrktNewsSearchTestApplication.class, args);
+        SpringApplication.run(OpenSearchCICDTest.class, args);
     }
 
 }
