75f9dd7982d579fbcdd7b56e2bec62c68871112d
JaewonLee <jlee38266@gmail.com>
2024-07-01 02:53:49 +0900

    기록용 커밋

--- src/main/java/githubActionTest/config/OpenSearchConfig.java
+++ src/main/java/githubActionTest/config/OpenSearchConfig.java
@@ -1,7 +1,11 @@
 package githubActionTest.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.opensearch.client.RestClient;
+import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +14,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenSearchConfig {
 
-    @Value("${opensearch.host}")
-    private String opensearchHost;
+    @Value("${spring.opensearch.host}")
+    private String host;
 
-    @Value("${opensearch.port}")
-    private int opensearchPort;
+    @Value("${spring.opensearch.port}")
+    private int port;
+
+    @Value("${spring.opensearch.username}")
+    private String username;
+
+    @Value("${spring.opensearch.password}")
+    private String password;
 
     @Bean
     public RestHighLevelClient restHighLevelClient() {
-        return new RestHighLevelClient(
-                RestClient.builder(new HttpHost(opensearchHost, opensearchPort, "http"))
-        );
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, "http"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(builder);
     }
 }
