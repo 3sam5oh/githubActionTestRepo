717bbaf5797b04195908ddcef7659a91c3d8b369
Lee Jaewon <jlee38266@gmail.com>
2024-07-01 17:31:07 +0900

    aws ec2 생성 테스트

--- main.tf
+++ main.tf
@@ -2,47 +2,56 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_opensearch_domain" "search_engine" {
-  domain_name           = "search-engine"
-  engine_version        = "OpenSearch_2.13" # OpenSearch 버전을 명시합니다.
+# resource "aws_opensearch_domain" "search_engine" {
+#   domain_name           = "search-engine"
+#   engine_version        = "OpenSearch_2.13"
+#
+#   cluster_config {
+#     instance_type = "t2.small.search"
+#     instance_count = 1
+#   }
+#
+#   ebs_options {
+#     ebs_enabled = true
+#     volume_size = 10
+#     volume_type = "gp2"
+#   }
+#
+#   access_policies = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "*"
+#       },
+#       "Action": "es:*",
+#       "Resource": "arn:aws:es:ap-northeast-2:123456789012:domain/search-engine/*",
+#       "Condition": {
+#         "IpAddress": {
+#           "aws:SourceIp": "0.0.0.0/0"
+#         }
+#       }
+#     }
+#   ]
+# }
+# EOF
+#
+#   advanced_options = {
+#     "rest.action.multi.allow_explicit_index" = "true"
+#   }
+#
+#   tags = {
+#     Domain = "search-engine"
+#   }
+# }
 
-  cluster_config {
-    instance_type = "t2.small.search"  # 프리 티어 인스턴스 유형
-    instance_count = 1
-  }
-
-  ebs_options {
-    ebs_enabled = true
-    volume_size = 10
-    volume_type = "gp2"
-  }
-
-  access_policies = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "es:*",
-      "Resource": "arn:aws:es:ap-northeast-2:123456789012:domain/search-engine/*",
-      "Condition": {
-        "IpAddress": {
-          "aws:SourceIp": "0.0.0.0/0"
-        }
-      }
-    }
-  ]
-}
-EOF
-
-  advanced_options = {
-    "rest.action.multi.allow_explicit_index" = "true"
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 18.04 AMI (region-specific)
+  instance_type = "t2.micro"
 
   tags = {
-    Domain = "search-engine"
+    Name = "terraform-example"
   }
 }
